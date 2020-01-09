d0739ffcb9b659e39c68e7d865ce3ac506634f3e
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-09 11:51:32 +0800

    测试样式

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,25 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import logo from '../logo.svg';
 import './App.less';
+import './common.less';
 import {Main} from "./main";
 
 const App: React.FC = () => {
   const [content, setContent] = useState("")
   const [count, setCount] = useState(0)
   let a = 1
+
   useEffect(() => {
     window.setInterval(() => {
       setCount(c => c + 1)
     }, 1000)
   }, [a])
+
   useEffect(() => {
-    const arr : any = document.querySelectorAll('style')
-    const lastStyle = arr[arr.length - 1]
-    setContent(lastStyle.innerHTML)
+    const arr : any = [].slice.call(document.querySelectorAll('style'), 0).map((i: any) => i.innerHTML)
+    setContent(arr.join('<p>--------------less-------------------</p>'))
   }, [count])
+
+  function func() {
+  }
+
+
   return (
     <div className="App">
-      <p>{content}</p>
+      <h2>less</h2>
+      <p dangerouslySetInnerHTML={{__html: content}} />
+      <h2>content</h2>
+      <div onClick={func}>test</div>
+      <div className="common-style">123</div>
       <Main/>
     </div>
   );
